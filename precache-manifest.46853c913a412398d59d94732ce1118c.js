44fad70dd7d879529755942b65d6ded4d9bacbe7
CarlosRubio9827 <rubiogallegoc@gmail.com>
2021-10-18 16:48:13 -0500

    Updates

---
+++ precache-manifest.46853c913a412398d59d94732ce1118c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe28ab3d403846568e740b7477ca06b",
+    "url": "/react-js-basic/index.html"
+  },
+  {
+    "revision": "3748706e96fdcc7ecd9c",
+    "url": "/react-js-basic/static/css/main.dbbdb381.chunk.css"
+  },
+  {
+    "revision": "88a6b8f4748323e21ca9",
+    "url": "/react-js-basic/static/js/2.37e152b4.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-js-basic/static/js/2.37e152b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3748706e96fdcc7ecd9c",
+    "url": "/react-js-basic/static/js/main.ad6388dc.chunk.js"
+  },
+  {
+    "revision": "5e58e0a987c85d527c08",
+    "url": "/react-js-basic/static/js/runtime-main.e5f78bee.js"
+  },
+  {
+    "revision": "cbc2e0d964c7ff8ad66cbd58963ed1ce",
+    "url": "/react-js-basic/static/media/Logo.cbc2e0d9.png"
+  }
+]);
